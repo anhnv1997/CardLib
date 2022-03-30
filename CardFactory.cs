21f1d0ee156840cbdbd4fa11a6e1a8444e5da927
Flick <anhnv@kztek.net>
2022-03-30 13:21:16 +0700

    First Commit

---
+++ CardFactory.cs
@@ -0,0 +1,33 @@
+﻿using KztekObject.Cards;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CardLibrary
+{
+    public class CardFactory
+    {
+        public enum EM_CardType
+        {
+            Proximity_Left,
+            ProximityRight,
+            Mifare,
+            ultra,
+        }
+
+        public static ICardController CreateCardController(int CardType)
+        {
+            switch (CardType)
+            {
+                case (int)EM_CardType.Mifare:
+                    return new MifareController(CardType);
+                case (int)EM_CardType.Proximity_Left:
+                    return new ProximityController(CardType);
+                case (int)EM_CardType.ProximityRight:
+                    return new ProximityController(CardType);
+                default:
+                    return null;
+            }
+        }
+    }
+}
---
+++ ICardController.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CardLibrary
+{
+    public interface ICardController
+    {
+        string GetCardHexNumber(string cardNumber);
+        int CardLen();
+    }
+}
